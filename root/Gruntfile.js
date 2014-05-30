f814a04e1ee983e92c01a8b3faf79b62540bbb9d
Teddy Chan <teddychan@gmail.com>
2014-05-30 12:50:25 +0800

    add jshint, ssh, promot, etc

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -2,47 +2,68 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    nodeunit: {
-      files: ['test/**/*_test.js'],
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib: {
-        src: ['lib/**/*.js']
-      },
-      test: {
-        src: ['test/**/*.js']
-      },
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
-      },
-    },
-  });
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			},
+			lib: {
+				src: ['lib/**/*.js']
+			},
+			test: {
+				src: ['test/**/*.js']
+			}
+		},
+		watch: {
+			gruntfile: {
+				files: '<%= jshint.gruntfile.src %>',
+				tasks: ['jshint:gruntfile']
+			},
+			lib: {
+				files: '<%= jshint.lib.src %>',
+				tasks: ['jshint:lib']
+			},
+			test: {
+				files: '<%= jshint.test.src %>',
+				tasks: ['jshint:test']
+			}
+		},
+		sshconfig: {
+			"ssh": grunt.file.readJSON('./key/ssh_key.json'),
+		},
+		shell: {
+			dev: {
+				options: {
+					stderr: true
+				},
+				command: 'npm start'
+			}
+		},
+		sshexec: {
+			deploy: {
+				command: 'git pull',
+				options: {
+					config: "ssh"
+				}
+			}
+		}
+	});
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-ssh');
+	grunt.loadNpmTasks('grunt-prompt');
+	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-ec2');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+	// Default task.
+	grunt.registerTask('default', ['jshint']);
+
+	grunt.registerTask('deploy', ['jshint', 'sshexec:deploy']);
 
 };
--- template.js
+++ template.js
@@ -13,15 +13,15 @@ exports.description = 'Create a Node.js module, including Nodeunit unit tests.';
 
 // Template-specific notes to be displayed before question prompts.
 exports.notes = '_Project name_ shouldn\'t contain "node" or "js" and should ' +
-  'be a unique ID not already in use at search.npmjs.org.';
+	'be a unique ID not already in use at search.npmjs.org.';
 
 // Template-specific notes to be displayed after question prompts.
 exports.after = 'You should now install project dependencies with _npm ' +
-  'install_. After that, you may execute project tasks with _grunt_. For ' +
-  'more information about installing and configuring Grunt, please see ' +
-  'the Getting Started guide:' +
-  '\n\n' +
-  'http://gruntjs.com/getting-started';
+	'install_. After that, you may execute project tasks with _grunt_. For ' +
+	'more information about installing and configuring Grunt, please see ' +
+	'the Getting Started guide:' +
+	'\n\n' +
+	'http://gruntjs.com/getting-started';
 
 // Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = '*';
@@ -29,51 +29,68 @@ exports.warnOn = '*';
 // The actual init template.
 exports.template = function(grunt, init, done) {
 
-  init.process({type: 'node'}, [
-    // Prompt for these values.
-    init.prompt('name'),
-    init.prompt('description'),
-    init.prompt('version'),
-    init.prompt('repository'),
-    init.prompt('homepage'),
-    init.prompt('bugs'),
-    init.prompt('licenses'),
-    init.prompt('author_name'),
-    init.prompt('author_email'),
-    init.prompt('author_url'),
-    init.prompt('node_version', '>= 0.8.0'),
-    init.prompt('main'),
-    init.prompt('npm_test', 'grunt nodeunit'),
-    {
-      name: 'travis',
-      message: 'Will this project be tested with Travis CI?',
-      default: 'Y/n',
-      warning: 'If selected, you must enable Travis support for this project in https://travis-ci.org/profile'
-    },
-  ], function(err, props) {
-    props.keywords = [];
-    props.devDependencies = {
-      'grunt-contrib-jshint': '~0.6.4',
-      'grunt-contrib-nodeunit': '~0.2.0',
-      'grunt-contrib-watch': '~0.5.3',
-    };
-    props.travis = /y/i.test(props.travis);
+	init.process({type: 'node'}, [
+		// Prompt for these values.
+		init.prompt('name'),
+		init.prompt('description'),
+		init.prompt('version', '0.0.1'),
+		init.prompt('repository'),
+		init.prompt('homepage'),
+		init.prompt('bugs'),
+		init.prompt('licenses', 'MIT'),
+		init.prompt('author_name'),
+		init.prompt('author_email'),
+		init.prompt('author_url'),
+		init.prompt('node_version', '>= 0.10.0'),
+		init.prompt('main'),
+		{
+			name: 'travis',
+			message: 'Will this project be tested with Travis CI?',
+			default: 'Y/n',
+			warning: 'If selected, you must enable Travis support for this project in https://travis-ci.org/profile'
+		},
+		{
+			name: 'jshint',
+			message: 'Will this project be tested with JSHint?',
+			default: 'Y/n',
+			warning: 'If selected, please change the setting in .jshintrc file'
+		}
+	], function(err, props) {
+		props.keywords = [];
+		props.devDependencies = {
+			"grunt": "^0.4.5",
+			"grunt-contrib-jshint": "^0.10.0",
+			"grunt-contrib-watch": "^0.6.1",
+			"grunt-ec2": "^0.8.1",
+			"grunt-prompt": "^1.1.0",
+			"grunt-shell": "^0.7.0",
+			"grunt-ssh": "^0.11.1"
+		};
+		props.travis = /y/i.test(props.travis);
 
-    // Files to copy (and process).
-    var files = init.filesToCopy(props);
-    if (!props.travis) { delete files['.travis.yml']; }
+		// Files to copy (and process).
+		var files = init.filesToCopy(props);
+		if (!props.travis) {
+			delete files['.travis.yml'];
+		}
 
-    // Add properly-named license files.
-    init.addLicenseFiles(files, props.licenses);
+		props.jshint = /y/i.test(props.jshint)
 
-    // Actually copy (and process) files.
-    init.copyAndProcess(files, props);
+		if (!props.jshint) {
+			delete files['.jshintrc'];
+		}
 
-    // Generate package.json file.
-    init.writePackageJSON('package.json', props);
+		// Add properly-named license files.
+		init.addLicenseFiles(files, props.licenses);
 
-    // All done!
-    done();
-  });
+		// Actually copy (and process) files.
+		init.copyAndProcess(files, props);
+
+		// Generate package.json file.
+		init.writePackageJSON('package.json', props);
+
+		// All done!
+		done();
+	});
 
 };
